6aa9843ad24b83e499e55d9537559a59bf2412b3
Your Name <you@example.com>
2022-12-11 13:55:12 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sqd',
+    version='0.0.1',
+    license='MIT',
+    author='xllwhoami',
+    author_email='xllwhoami@ukr.net',
+    packages=find_packages('sqd'),
+    package_dir={'': 'sqd'},
+    url='https://github.com/xllwhoami/sqd',
+    keywords='sqlite db coinfig database lite',
+    install_requires=[],
+
+)
